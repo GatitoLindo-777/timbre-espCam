2bcf0a3b31d2ed60fb5fc3933bbd6b0b5f30aee6
GatitoLindo-777 <gaspikatz2005@gmail.com>
2023-05-05 12:46:19 -0300

    codigo sin probar

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # timbre-espCam
 al presionar un boton se envia un mensaje con una foto de quien presiona a un chat de telegram
+
+
+
+links de investigacion:
+codigo para enviar a telegram: https://randomnerdtutorials.com/telegram-esp32-cam-photo-arduino/
+ESPCam PINOUT: https://randomnerdtutorials.com/esp32-cam-ai-thinker-pinout/
+ESP32 install: https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
--- timbre-espCam/timbre-espCam.ino
+++ timbre-espCam/timbre-espCam.ino
@@ -1,108 +1,254 @@
+// credenciales de wifi
+
+
 // Incluimos las librerias
-#include <WiFi.h> // WIFI para ESP32
-#include <WiFiClientSecure.h> 
-#include <UniversalTelegramBot.h>   // Universal Telegram Bot Library written by Brian Lough: https://github.com/witnessmenow/Universal-Arduino-Telegram-Bot
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#include "esp_camera.h"
+#include <UniversalTelegramBot.h>
 #include <ArduinoJson.h>
 
-// credenciales de wifi
-const char* ssid = "ORT-IoT";  // cambiar por red WIFI deseada
-const char* password = "OrtIOTnew22$2";  // cambiar por la clave de WIFI
+// Configuramos red wifi
+const char* ssid = "BJ2 2.4Ghz"; // Remplazar por el nombre de la red wifi
+const char* password = "Benitojuarez"; // Remplazar por la clave del wifi
 
-// Initialize Telegram BOT
-#define BOTtoken "2057747784:AAGmapbuxScYVtSGqdl8LhQrsKdt1rbeaBs"  // cambiar por token 
+// Inicializamos el BOT
+String BOTtoken = "6068641172:AAF_9siLYwZ-JFoX51Evfuh9RXk-jSB9UlI";  // Remplazar por el BotToken
+String CHAT_ID = "5388508527"; // Remplazar por el ID del usuario
 
-#define CHAT_ID "-526457647" //  cambiar por el chat id del usuario 
+bool sendPhoto = false;
 
-WiFiClientSecure client;
-UniversalTelegramBot bot(BOTtoken, client);
+WiFiClientSecure clientTCP;
+UniversalTelegramBot bot(BOTtoken, clientTCP);
 
+ #define BUTTON_PIN 4
+ //bool flashState = LOW;
 
-int botRequestDelay = 1000; /// intervalo de tiempo entre lectras del bot (en ms)
-unsigned long lastTimeBotRan; /// ultimo vez que el bot leyo 
+//Checks for new messages every 1 second.
+int botRequestDelay = 1000;
+unsigned long lastTimeBotRan;
 
-const int ledPin = 2; /// pin led 
-bool ledState = LOW;
+// Seteamos los pines del CAMERA_MODEL_AI_THINKER
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
 
-void setup() {
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+void setup(){
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); 
+  // Init Serial Monitor
   Serial.begin(9600);
 
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, ledState);
-  
-  // Conecto el wifi Wi-Fi
+  // Seteamos el boton como input
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  // Config and init the camera
+  configInitCamera();
+
+  // Connect to Wi-Fi
   WiFi.mode(WIFI_STA);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
   WiFi.begin(ssid, password);
-  client.setCACert(TELEGRAM_CERTIFICATE_ROOT); 
-  Serial.println("Connecting to WiFi..");
-  
-  ///bloquea el programa si no se puede conectar a internet 
-  while (WiFi.status() != WL_CONNECTED) {   
-    delay(1000);
-    Serial.println(".");
+  clientTCP.setCACert(TELEGRAM_CERTIFICATE_ROOT); // Add root certificate for api.telegram.org
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
   }
-  
-  // Print ESP32 Local IP Address
-  Serial.println(WiFi.localIP());
-  bot.sendMessage(CHAT_ID, "Bot Hola mundo", "");
+  Serial.println();
+  Serial.print("ESP32-CAM IP Address: ");
+  Serial.println(WiFi.localIP()); 
 }
 
 void loop() {
+  if (digitalRead(BUTTON_PIN == 0) {
+    Serial.println("Preparing photo");
+    sendPhotoTelegram(); 
+    delay(1000); // REMPLAZAR CON ANTIRREBOT (NO SEAS UN HDP @YO)
+  }
 
-  if (millis() > lastTimeBotRan + botRequestDelay) { // si el tiempo que trancurrio es mayor al delay seteado leemos de vuelta
+  // Si queremos leer mensajes incluimos esta seccion para que leea ca el tiempo seteado
+  /*if (millis() > lastTimeBotRan + botRequestDelay)  {
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
-
-    while(numNewMessages) {
-      Serial.println("Veo los msj nuevos");
+    while (numNewMessages) {
+      Serial.println("got response");
       handleNewMessages(numNewMessages);
       numNewMessages = bot.getUpdates(bot.last_message_received + 1);
     }
     lastTimeBotRan = millis();
-  
-  }
+  }*/
 }
 
-// funcion que se comunica con telegram
-void handleNewMessages(int numNewMessages) {
-  Serial.println("Mensaje nuevo");
-  Serial.println(String(numNewMessages));
+// Funcion para configurar la camara e inicializarla
+void configInitCamera(){
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
 
-  for (int i=0; i<numNewMessages; i++) {
-    // inicio de verificacion
+  //init with high specs to pre-allocate larger buffers
+  // SI la camara lo permite la inicialisamos con caracteristicas altas
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;  //0-63 cuanto menor el numero mayor calidad de la camara
+    config.fb_count = 2;
+  } else { 
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;  //0-63 cuanto menor el numero mayor calidad de la camara
+    config.fb_count = 1;
+  }
+  
+  // Inizialisacion de la camara
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed ", err);
+    delay(1000);
+    ESP.restart();
+  }
+
+  // Drop down frame size for higher initial frame rate
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_framesize(s, FRAMESIZE_CIF);  // UXGA|SXGA|XGA|SVGA|VGA|CIF|QVGA|HQVGA|QQVGA
+}
+
+
+// Recibimos los nuebos mensajes
+void handleNewMessages(int numNewMessages) {
+  Serial.print("Handle New Messages: ");
+  Serial.println(numNewMessages);
+
+  for (int i = 0; i < numNewMessages; i++) {
     String chat_id = String(bot.messages[i].chat_id);
-    if (chat_id != CHAT_ID){   ////si el id no corresponde da error . en caso de que no se quiera comprobar el id se debe sacar esta parte 
+    if (chat_id != CHAT_ID){
       bot.sendMessage(chat_id, "Unauthorized user", "");
       continue;
     }
-    ///fin de verificacion
-
-    /*// imprime el msj recibido 
+    
+    // Print the received message
     String text = bot.messages[i].text;
-    Serial.println(text);
-
+    Serial.println(text); // Guardamos el mensaje en la variable "text"
+    
     String from_name = bot.messages[i].from_name;
-
-    /// si rebice /led on enciende el led 
-    if (text == "/led_on") {  
-      bot.sendMessage(chat_id, "LED state set to ON", "");
-      ledState = HIGH;
-      digitalWrite(ledPin, ledState);
+    if (text == "/start") {
+      String welcome = "Welcome , " + from_name + "\n";
+      //welcome += "Use the following commands to interact with the ESP32-CAM \n";
+      // Si desemaos que nuestro programa haga algo con mensajes recibidos los podemos incluir aqui
+      bot.sendMessage(CHAT_ID, welcome, "");
     }
+}
+
+// Funcion que se encarga de enviarle la foto al BOT
+String sendPhotoTelegram() {
+  const char* myDomain = "api.telegram.org";
+  String getAll = "";
+  String getBody = "";
+
+  camera_fb_t * fb = NULL;
+  fb = esp_camera_fb_get();  
+  if(!fb) {
+    Serial.println("Camera capture failed");
+    delay(1000);
+    ESP.restart();
+    return "Camera capture failed";
+  }  
+  
+  Serial.println("Connect to " + String(myDomain));
+
+
+  if (clientTCP.connect(myDomain, 443)) {
+    Serial.println("Connection successful");
     
-    if (text == "/led_off") {
-      bot.sendMessage(chat_id, "LED state set to OFF", "");
-      ledState = LOW;
-      digitalWrite(ledPin, ledState);
-    }
+    String head = "--RandomNerdTutorials\r\nContent-Disposition: form-data; name=\"chat_id\"; \r\n\r\n" + CHAT_ID + "\r\n--RandomNerdTutorials\r\nContent-Disposition: form-data; name=\"photo\"; filename=\"esp32-cam.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n";
+    String tail = "\r\n--RandomNerdTutorials--\r\n";
+
+    uint16_t imageLen = fb->len;
+    uint16_t extraLen = head.length() + tail.length();
+    uint16_t totalLen = imageLen + extraLen;
+  
+    clientTCP.println("POST /bot"+BOTtoken+"/sendPhoto HTTP/1.1");
+    clientTCP.println("Host: " + String(myDomain));
+    clientTCP.println("Content-Length: " + String(totalLen));
+    clientTCP.println("Content-Type: multipart/form-data; boundary=RandomNerdTutorials");
+    clientTCP.println();
+    clientTCP.print(head);
+  
+    uint8_t *fbBuf = fb->buf;
+    size_t fbLen = fb->len;
+    for (size_t n=0;n<fbLen;n=n+1024) {
+      if (n+1024<fbLen) {
+        clientTCP.write(fbBuf, 1024);
+        fbBuf += 1024;
+      }
+      else if (fbLen%1024>0) {
+        size_t remainder = fbLen%1024;
+        clientTCP.write(fbBuf, remainder);
+      }
+    }  
     
-    if (text == "/state") {
-      if (digitalRead(ledPin)){
-        bot.sendMessage(chat_id, "LED is ON", "");
+    clientTCP.print(tail);
+    
+    esp_camera_fb_return(fb);
+    
+    int waitTime = 10000;   // timeout 10 seconds
+    long startTimer = millis();
+    boolean state = false;
+    
+    while ((startTimer + waitTime) > millis()){
+      Serial.print(".");
+      delay(100);      
+      while (clientTCP.available()) {
+        char c = clientTCP.read();
+        if (state==true) getBody += String(c);        
+        if (c == '\n') {
+          if (getAll.length()==0) state=true; 
+          getAll = "";
+        } 
+        else if (c != '\r')
+          getAll += String(c);
+        startTimer = millis();
       }
-      else{
-        bot.sendMessage(chat_id, "LED is OFF", "");
-      }
+      if (getBody.length()>0) break;
     }
-  }*/
-
-
+    clientTCP.stop();
+    Serial.println(getBody);
+  }
+  else {
+    getBody="Connected to api.telegram.org failed.";
+    Serial.println("Connected to api.telegram.org failed.");
+  }
+  return getBody;
 }
